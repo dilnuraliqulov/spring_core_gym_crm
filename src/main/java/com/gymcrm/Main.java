0fd7b75ff36c9544b6e7b81e2acaa65ad6a22b71
Dilnur Aliqulov <d.aliqulov@newuu.uz>
2025-10-31 10:25:12 +0500

    Add Spring Gym CRM project setup

---
+++ src/main/java/com/gymcrm/Main.java
@@ -0,0 +1,14 @@
+package com.gymcrm;
+
+import com.gymcrm.config.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        System.out.println("Spring Context Loaded Successfully!");
+
+    }
+}
