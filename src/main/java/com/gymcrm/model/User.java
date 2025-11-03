3fbc6e6f6dcf732a0334e6c95c938bfcf3393949
Dilnur Aliqulov <d.aliqulov@newuu.uz>
2025-11-03 20:11:10 +0500

    Added @EqualsAndHashCode(of='id') to all entity classes

--- src/main/java/com/gymcrm/model/User.java
+++ src/main/java/com/gymcrm/model/User.java
@@ -2,11 +2,13 @@ package com.gymcrm.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(of="id")
 public class User {
    private Long id;
    private String firstName;
