5d168415317c7ae844535bfd3a4990b47410ef3d
Dilnur Aliqulov <d.aliqulov@newuu.uz>
2025-11-16 17:21:55 +0500

    Update hibernate configuration: -Created a seperate bean holding connection properties -Refactored HibernateConfig

--- src/main/java/com/gymcrm/config/HibernateConfig.java
+++ src/main/java/com/gymcrm/config/HibernateConfig.java
@@ -1,11 +1,9 @@
 package com.gymcrm.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
@@ -16,34 +14,11 @@ import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
+@RequiredArgsConstructor
 public class HibernateConfig {
 
     // Load YAML properties
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
-        yaml.setResources(new ClassPathResource("application.yaml"));
-
-        PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
-        Properties props = yaml.getObject();
-        if (props == null) {
-            throw new IllegalStateException("Failed to load application.yml from classpath!");
-        }
-        configurer.setProperties(props);
-        return configurer;
-    }
-
-    @Value("${database.driverClassName}")
-    private String driverClassName;
-
-    @Value("${database.url}")
-    private String url;
-
-    @Value("${database.username}")
-    private String username;
-
-    @Value("${database.password}")
-    private String password;
+    private final DatabaseProperties dbProps;
 
     @Value("${hibernate.dialect}")
     private String dialect;
@@ -57,25 +32,26 @@ public class HibernateConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-
+        dataSource.setDriverClassName(dbProps.getDriverClassName());
+        dataSource.setUrl(dbProps.getUrl());
+        dataSource.setUsername(dbProps.getUsername());
+        dataSource.setPassword(dbProps.getPassword());
         return dataSource;
     }
+
 
     @Bean
     public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
         sessionFactory.setPackagesToScan("com.gymcrm.model");
+
         Properties props = new Properties();
         props.setProperty("hibernate.dialect", dialect);
         props.setProperty("hibernate.show_sql", showSql);
         props.setProperty("hibernate.hbm2ddl.auto", hbm2ddl);
-        sessionFactory.setHibernateProperties(props);
 
+        sessionFactory.setHibernateProperties(props);
         return sessionFactory;
     }
 
