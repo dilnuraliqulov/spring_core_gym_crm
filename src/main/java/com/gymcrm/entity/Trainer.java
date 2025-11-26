dd31c3ac4e51d628a70dbb4d4be0eb45a12e55f3
Dilnur Aliqulov <d.aliqulov@newuu.uz>
2025-11-26 17:52:40 +0500

    Implement core entities: Trainee, Trainer, Training; update TrainingType
    - Added Trainee, Trainer, and Training entity classes with appropriate JPA mappings
    - Modified TrainingType entity to reflect relationships with Trainer and Training
    - Prepared entities for database persistence and future business logic

--- src/main/java/com/gymcrm/entity/Trainer.java
+++ src/main/java/com/gymcrm/entity/Trainer.java
@@ -1,39 +1,34 @@
 package com.gymcrm.entity;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.Date;
 import java.util.List;
 
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
-@Entity
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Table(name = "trainees")
-public class Trainee {
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "trainers")
+public class Trainer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @EqualsAndHashCode.Include
     private Long id;
 
-    @Column(name = "date_of_birth")
-    private Date dateOfBirth;
-
-    @Column(name = "address")
-    private String address;
-
-    @OneToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "user_id",referencedColumnName = "id")
+    @OneToOne(optional = true ,cascade = CascadeType.ALL)
+    @ToString.Exclude
     private User user;
 
-    @OneToMany(mappedBy = "trainee", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "trainer", cascade = CascadeType.ALL)
     @ToString.Exclude
     private List<Training> trainings;
+
+
 
 }
--- src/main/java/com/gymcrm/entity/TrainingType.java
+++ src/main/java/com/gymcrm/entity/TrainingType.java
@@ -1,6 +1,5 @@
 package com.gymcrm.entity;
 
-import com.gymcrm.model.Training;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
