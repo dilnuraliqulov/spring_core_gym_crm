d7ae694da523e37043b51aa7b64fa2741db9a657
Dilnur Aliqulov <d.aliqulov@newuu.uz>
2025-11-26 15:28:46 +0500

    Implement Trainee implementation: -Added Trainee entity with id,dateOfBirth,address fields -Specified the relationship with user table

---
+++ src/main/java/com/gymcrm/entity/Trainee.java
@@ -0,0 +1,34 @@
+package com.gymcrm.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "trainees")
+public class Trainee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "date_of_birth")
+    private Date dateOfBirth;
+
+    @Column(name = "address")
+    private String address;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "user_id",referencedColumnName = "id")
+    private User user;
+
+}
