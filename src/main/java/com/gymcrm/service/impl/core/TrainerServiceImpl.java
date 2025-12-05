1cf7b9dc278d5f9d94e0bceff0244e34a1f87772
Dilnur Aliqulov <d.aliqulov@newuu.uz>
2025-12-05 16:42:16 +0500

    Updated services

--- src/main/java/com/gymcrm/service/impl/core/TrainerServiceImpl.java
+++ src/main/java/com/gymcrm/service/impl/core/TrainerServiceImpl.java
@@ -5,11 +5,13 @@ import com.gymcrm.model.Trainer;
 import com.gymcrm.service.TrainerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class TrainerServiceImpl implements TrainerService {
 
--- src/main/java/com/gymcrm/service/impl/core/TrainingServiceImpl.java
+++ src/main/java/com/gymcrm/service/impl/core/TrainingServiceImpl.java
@@ -5,11 +5,13 @@ import com.gymcrm.model.Training;
 import com.gymcrm.service.TrainingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class TrainingServiceImpl implements TrainingService {
 
